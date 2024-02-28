a3ea5816e14a567809ef5f71e9b3f30064d57472
geeksami1 <samigeekslogicity@gmail.com>
2024-02-29 03:23:49 +0500

    teacher portal

---
+++ src/ui-component/cards/Teaching/TeachingTable.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Box, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Avatar, Button } from '@mui/material';
+
+const TeachingTable = ({ data }) => {
+  return (
+    <Box>
+      <TableContainer component={Paper}>
+        <Table aria-label="teaching table">
+          <TableHead sx={{display: 'none'}}>
+            <TableRow>
+              <TableCell style={{ color: 'black' }}>Name</TableCell>
+              <TableCell style={{ color: 'black' }}>Class</TableCell>
+              <TableCell style={{ color: 'black' }}>Subject</TableCell>
+              <TableCell style={{ color: 'black' }}>Chapter</TableCell>
+              <TableCell style={{ color: 'black' }}>Percentage</TableCell>
+              <TableCell style={{ color: 'black' }}>Action</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.map((row, index) => (
+              <React.Fragment key={index}>
+                <TableRow>
+                  <TableCell component="th" scope="row" style={{ color: 'black' }}>
+                    <Box display="flex" alignItems="center">
+                      <Avatar style={{ marginRight: '8px' }} src={row.avatar} />
+                      {row.name}
+                    </Box>
+                  </TableCell>
+                  <TableCell style={{ color: 'black' }}>{row.class}</TableCell>
+                  <TableCell style={{ color: 'black' }}>{row.subject}</TableCell>
+                  <TableCell style={{ color: 'black' }}>{row.chapter}</TableCell>
+                  <TableCell style={{ color: 'black' }}>{row.percentage}</TableCell>
+                  <TableCell style={{ color: 'black' }}>
+                  <Button variant="outlined" color="primary" style={{marginLeft: '10px'}}>{row.action}</Button>
+                  </TableCell>
+                </TableRow>
+                {index !== data.length - 1 && (
+                  <TableRow style={{ height: '10px' , border: 'none' }}>
+                    <TableCell colSpan={7} />
+                  </TableRow>
+                )}
+              </React.Fragment>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
+  );
+};
+
+export default TeachingTable;
--- src/views/dashboard/Teacher/index.js
+++ src/views/dashboard/Teacher/index.js
@@ -1,13 +1,13 @@
 import { useEffect, useState } from 'react';
 
 // material-ui
-import { Grid } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
 
 // project imports
 
 import { gridSpacing } from 'store/constant';
-import EarningCard from '../Default/EarningCard';
-import TotalOrderLineChartCard from '../Default/TotalOrderLineChartCard';
+// import EarningCard from '../Default/EarningCard';
+// import TotalOrderLineChartCard from '../Default/TotalOrderLineChartCard';
 import TotalIncomeDarkCard from '../Default/TotalIncomeDarkCard';
 import TotalIncomeLightCard from '../Default/TotalIncomeLightCard';
 import TotalGrowthBarChart from '../Default/TotalGrowthBarChart';
@@ -17,6 +17,7 @@ import img1 from '../../../assets/images/teacher1.png';
 import img2 from '../../../assets/images/teacher3.png';
 import img3 from '../../../assets/images/teacher2.png';
 import img4 from '../../../assets/images/teacher5.png';
+import TeachingTable from 'ui-component/cards/Teaching/TeachingTable';
 // ==============================|| DEFAULT TeacherDashboard ||============================== //
 
 const TeacherDashboard = () => {
@@ -25,9 +26,16 @@ const TeacherDashboard = () => {
     setLoading(false);
   }, []);
 
+  const data = [
+    {avatar: img3, name: 'John Doe', class: '10th', subject: 'Math',chapter: '5', percentage: '90%', action: "Test", color: '#7F49F2', },
+    {avatar: img2, name: 'Jane Smith', class: '12th', subject: 'Science', chapter: '3' ,percentage: '85%', action:"Quiz", color: '#E9C030'},
+    {avatar: img1, name: 'Jakobs Robert', class: '11th', subject: 'Biology', chapter: '6' ,percentage: '78%', action:"Quiz", color: '#EA794A'},
+  ];
+
   return (
     <Grid container spacing={gridSpacing}>
       <Grid item xs={12}>
+        {/* TEACHING DATA CARD START */}
         <Grid container spacing={gridSpacing}>
           <Grid item lg={3} md={3} sm={3} xs={3}>
             <TeachingData color={'#7F49F2'} title={'Total Students'} number={'20'} img={img1} />
@@ -42,15 +50,17 @@ const TeacherDashboard = () => {
             <TeachingData color={'#3BC6DF'} title={'Total Class'} number={'25'} img={img3} />
           </Grid>
         </Grid>
+        {/* TEACHING DATA CARD END */}
       </Grid>
       <Grid item xs={12}>
         <Grid container spacing={gridSpacing}>
-          <Grid item lg={4} md={6} sm={6} xs={12}>
-            <EarningCard isLoading={isLoading} />
-          </Grid>
-          <Grid item lg={4} md={6} sm={6} xs={12}>
-            <TotalOrderLineChartCard isLoading={isLoading} />
-          </Grid>
+         
+            <Grid item lg={8} md={8} sm={8} xs={12}>
+              <Typography variant="h2" my={2} sx={{ color: '#00000082' }}>
+                Pending homework
+              </Typography>
+            <TeachingTable  data={data} isLoading={isLoading}/>
+          </Grid> 
           <Grid item lg={4} md={12} sm={12} xs={12}>
             <Grid container spacing={gridSpacing}>
               <Grid item sm={6} xs={12} md={6} lg={12}>
